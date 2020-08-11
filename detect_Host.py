80e275cb66f425be08aa563d642d21c16e2f44b1
MoonHyun Kim <moonhyun.dev@gmail.com>
2020-08-11 09:48:12 +0900

    Update detect_Host.py

--- detect_Host.py
+++ detect_Host.py
@@ -1,3 +1,14 @@
+"""
+Cloud Simulator
+
+ • Author : Moonhyun kim
+ • Date : May 15 , 2020
+ • Last modified date : Jul 5, 2020
+
+ • Department of Computer Science at Chungbuk National University
+"""
+
+
 def static_threshold (Host_list) :
     Upper_Threshold = 80.0
     lower_Threshold = 20.0
